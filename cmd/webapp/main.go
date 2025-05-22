3c4f33ec693ce7017d70f56f49174b7c955118a7
Ajay Sharma <ajay.sharma1984@gmail.com>
2025-05-21 21:39:16 -0400

    build changes

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -2,29 +2,13 @@ package main
 
 import (
 	"github.com/ajaxe/mc-manager/internal/client"
-	"github.com/ajaxe/mc-manager/internal/pages"
-	"github.com/ajaxe/mc-manager/internal/server"
-	"github.com/labstack/echo/v4"
 	"github.com/maxence-charriere/go-app/v10/pkg/app"
 )
 
 func main() {
-	// This is the entry point for the web application.
-	// The main function will initialize the application and start the server.
-	app.Route("/", func() app.Composer { return &pages.HomePage{} })
-	app.Route("/worlds", func() app.Composer { return &pages.HomePage{} })
-	app.Route("/worlds/add", func() app.Composer { return &pages.WorldAddPage{} })
-	app.Route("/launches", func() app.Composer { return &pages.WorldLaunchPage{} })
-	app.Route("/setup", func() app.Composer { return &pages.SetupPage{} })
+	Frontend()
 
 	app.RunWhenOnBrowser()
 
-	s := server.NewBackendApi()
-
-	s.GET("/*", func(c echo.Context) error {
-		client.GoAppHandler.ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
-
-	server.Start(s)
+	Backend(client.GoAppHandler)
 }
